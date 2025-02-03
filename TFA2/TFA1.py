a6f463609789783b90d7d8b496d04a17e3d5cded
Yanela-1 <ryeyanela30@gmail.com>
2025-02-03 10:44:50 +0800

    finish

--- TFA2/TFA1.py
+++ TFA2/TFA1.py
@@ -1,5 +1,4 @@
 #3.1
-'''
 fname = input("Enter your first name: ")
 lname = input("Enter your last name: ")
 age = input("Enter your age: ")
@@ -8,19 +7,16 @@ print("Full Name: ", fname + " " + lname )
 print("Sliced Name: ", fname[0:2] )
 greetings = "Hello, {}! Welcome. You are {} years old."
 print(greetings.format(fname[0:2], age))
-'''
+
 #3.2
-'''
 fname = input("Enter your first name: ")
 lname = input("Enter your last name: ")
 print("Full Name: ", fname + " " + lname)
 print("Full Name (Upper Case) :", (fname + " " + lname).upper())
 print("Full Name (Lower Case) :", (fname + " " + lname).lower())
 print("Length of Full name: ", len(fname + " " + lname))
-'''
 
 #3.3
-'''
 lname = input("Enter last name: ")
 fname = input("Enter first name: ")
 age = input("Enter age: ")
@@ -36,10 +32,11 @@ f.write("Course: " + course + "\n")
 f.close()
 
 print("Student information has been saved to 'students.txt'.")
-'''
 
 #3.4
 f = open("TFA2\students.txt", "r")
 line = f.readline()
-print(line)
+while line!='':
+    print(line)
+    line = f.readline()
 f.close()
