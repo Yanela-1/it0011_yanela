463fcf0584d655c1f3a9c0e200443a06bd22638f
Yanela-1 <ryeyanela30@gmail.com>
2025-02-03 10:04:39 +0800

    activities

---
+++ TFA1/TFA1.py
@@ -0,0 +1,22 @@
+#3.1
+'''
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+age = input("Enter your age: ")
+
+print("Full Name: ", fname + " " + lname )
+print("Sliced Name: ", fname[0:2] )
+greetings = "Hello, {}! Welcome. You are {} years old."
+print(greetings.format(fname[0:2], age))
+'''
+#3.2
+'''
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+print("Full Name: ", fname + " " + lname)
+print("Full Name (Upper Case) :", (fname + " " + lname).upper())
+print("Full Name (Lower Case) :", (fname + " " + lname).lower())
+print("Length of Full name: ", len(fname + " " + lname))
+'''
+
+#3.3
